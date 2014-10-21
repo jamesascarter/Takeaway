7d5fea56d591e0e0ee1a412bbf7404f70f97beec
jamesascarter <jamesascarter@gmail.com>
2014-10-21 19:26:00 +0100

    attempted to test txt message. Added gem for Webmock. Test still to be complteed

---
+++ spec/message_spec.rb
@@ -0,0 +1,13 @@
+require 'message'
+require 'webmock/rspec'
+
+WebMock.disable_net_connect!(allow_localhost: true)
+
+describe Message do
+
+	it "should send a text messaage via Twilio" do
+
+	end
+
+end
+
